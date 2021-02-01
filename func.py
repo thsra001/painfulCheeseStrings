8c05e366abedfef52315f69fd3df48ad8e8875eb
Thsra001 Gameing <tharunsritharun@gmail.com>
2021-02-01 10:15:33 +0000

    idk

--- func.py
+++ func.py
@@ -10,3 +10,18 @@ def typew(lmao):
  print(lmao)
 
 def tutorial():
+ typew("this is little tommyboi")
+ print("       |๏ ͜ʖ๏|")
+ e=input("")
+ cmd = 0
+ while not cmd == "!feed":
+  cls()
+  typew("feed him with !𝙛𝙚𝙚𝙙")
+  print("       |๏ ͜ʖ๏|")
+  cmd = input("")
+ while not cmd == "!bath":
+ cls()
+ typew("good, now give him a bath with !𝙗𝙖𝙩𝙝")
+ cmd = input("")
+ cls()
+ typew("great, now you are ready to take care of tommyboi!")
